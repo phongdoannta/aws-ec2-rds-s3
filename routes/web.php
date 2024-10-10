b4dc0d29c65905ae88c28419c8ddf6ee265e5f81
Doan Le Quoc Phong <phongdlq.nta@gmail.com>
2024-10-10 10:28:25 +0700

    [updated] upload s3

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,17 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\AWSController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::group(['as' => 'aws.', 'prefix' => 'aws'], function () {
+    Route::get('list', [AWSController::class, 'index'])->name('list');
+    Route::post('s3-upload', [AWSController::class, 'uploadFileS3'])->name('upload');
+    Route::post('s3-delete', [AWSController::class, 'deleteFileS3'])->name('delete');
 });
 
 Route::get('/dashboard', function () {
